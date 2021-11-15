df683e4ea224bdcf8771eb04873f6f0214e32d85
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-11-16 01:27:00 +0200

    add lesson13 task1 done

---
+++ lesson13/task1/index.test.js
@@ -0,0 +1,14 @@
+it ('17 equals 17', () => {
+	expect(17).toEqual(17);
+});
+
+it ('17 equals 18', () => {
+	expect(17).not.toEqual(18);
+});
+
+const getEvenNumbers = arr => arr.filter(elem => elem % 2 === 0);
+
+it('to show all even number from array', () => {
+	const result = getEvenNumbers([1, 2, 3, 4, 5]);
+	expect(result).toEqual([2, 4]);
+});
