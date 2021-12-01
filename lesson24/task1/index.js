5003401f5841d9c740a8e76b03bfabb81ba7186c
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-01 14:46:50 +0200

    lesson24 task1

---
+++ lesson24/task1/index.js
@@ -0,0 +1,12 @@
+const nameOfDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const startDay = new Date(date).getDate();
+  const futureDay = new Date(date).setDate(startDay + days);
+
+  return nameOfDays[new Date(futureDay).getDay()];
+};
+
+// test data
+// const result = dayOfWeek(new Date(2021, 11, 3), 14);
+// console.log(result);
--- lesson9/hw4/script.js
+++ lesson9/hw4/script.js
@@ -1,25 +1,14 @@
-const getPeople = obj => Object.entries(obj)
-								.map(el => Object.values(el[1]))
-								.flat()
-								.map(name => Object.values(name))
-								.flat();
+const getPeople = obj =>
+  Object.entries(obj)
+    .map(el => Object.values(el[1]))
+    .flat()
+    .map(name => Object.values(name))
+    .flat();
 
+const rooms = {
+  room1: [{ name: 'Jack' }, { name: 'Andrey' }, { name: 'Ann' }, { name: 'Vasyl' }],
+  room2: [{ name: 'Dan' }],
+  room3: [{ name: 'Denis' }, { name: 'Max' }, { name: 'Alex' }],
+};
 
- const rooms = {
-	room1: [
-		{ name: 'Jack' },
-		{ name: 'Andrey' },
-		{ name: 'Ann' },
-		{ name: 'Vasyl' },
-	],
-	room2: [
-		{ name: 'Dan' },
-	],
-	room3: [
-		{ name: 'Denis' },
-		{ name: 'Max' },
-		{ name: 'Alex' },
-	],
- };
-
- console.log(getPeople(rooms));
+console.log(getPeople(rooms));
