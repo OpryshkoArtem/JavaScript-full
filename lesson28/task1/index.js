f089c7d038e266022e4b12dd2522dd7ecf281cfa
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-06 01:59:22 +0200

    lesson28 task1 done

---
+++ lesson28/task1/index.js
@@ -0,0 +1,30 @@
+const calc = startValue => {
+  let res = startValue;
+  const calulator = {
+    add(value) {
+      res += value;
+      return this;
+    },
+    multi(value) {
+      res *= value;
+      return this;
+    },
+    div(value) {
+      res /= value;
+      return this;
+    },
+    substract(value) {
+      res -= value;
+      return this;
+    },
+    result() {
+      return res;
+    },
+  };
+
+  return calulator;
+};
+
+// test data
+// const result = calc(3).add(2).multi(4).div(10).substract(5).result();
+// console.log(result);
