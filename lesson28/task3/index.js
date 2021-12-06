4322c941484ea2c3022f1ddc0f120a18db45e507
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-06 02:47:45 +0200

    task3 done

---
+++ lesson28/task3/index.js
@@ -0,0 +1,13 @@
+export const mult = num1 => num2 => num1 * num2;
+
+export const twice = mult(2);
+
+export const triple = mult(3);
+
+// test data
+
+// const test = mult(5)(4);
+// const test2 = twice(7);
+// const test3 = triple(9);
+
+// console.log(test, test2, test3);
