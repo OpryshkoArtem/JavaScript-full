489f1773c4667899d9e27708b527afd1ac1c31f5
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-08 20:03:56 +0200

    hw 1, hw2 done

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -2,16 +2,16 @@ const getRandomNumber = (from, to) => from + Math.random() * (to - from);
 
 const request = url =>
   new Promise(resolve => {
-    const randomDelay = getRandomNumber(1000, 3000);
+    const randomeDelay = getRandomNumber(1000, 3000);
     setTimeout(() => {
       resolve({
         userData: {
           name: 'Tom',
-          age: 46,
+          age: 17,
         },
-        sourse: url,
+        source: url,
       });
-    }, randomDelay);
+    }, randomeDelay);
   });
 
 const servers = ['https://server.com/us', 'https://server.com/eu', 'https://server.com/au'];
@@ -25,4 +25,4 @@ export const getUserASAP = userId => {
 };
 
 // test data
-// getUserASAP('user-id-1').then(res => console.log(res));
+getUserASAP('user-id-1').then(res => console.log(res));
