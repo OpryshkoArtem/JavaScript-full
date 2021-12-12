28af4590cd27e27f6d9147d661859e089e4acfbd
OpryshkoArtem <opryshkoartemua@gmail.com>
2021-12-12 21:33:33 +0200

    try

--- lesson33/hw1/index.js
+++ lesson33/hw1/index.js
@@ -14,10 +14,10 @@ export function getTaskById(taskId) {
 //
 // test data
 // examples
-getTasksList().then(tasksList => {
-  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-});
+// getTasksList().then(tasksList => {
+//   console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
+// });
 
-getTaskById('2').then(taskData => {
-  console.log(taskData); // {'id':'2', 'done':true ... }
-});
+// getTaskById('2').then(taskData => {
+//   console.log(taskData); // {'id':'2', 'done':true ... }
+// });
